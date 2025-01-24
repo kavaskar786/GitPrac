2f0dd87e3811111d6fe8e3ec6f07620d3ec0770a
kavaskar786 <kavaskar.s@mca.christuniversity.in>
2025-01-24 14:58:44 +0530

    feat: add butterfly and rombus patterns, implement square structure in test.py

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-for i in range(1, 4):  # Outer loop for rows
-    for j in range(3, 1, -1):  # Inner loop, counting down from 3 to 2
-        print("*" * j + " " * i + "*" * j)
+
+# butterfly
+for i in range(1, 4): 
+    print("*"*i + " "*(6-i*2) + "*"*i)
+for i in range(2,0,-1): 
+    print("*"*i + " "*(6-i*2) + "*"*i)
+
+
+
+#square
+print("----------------------------------- SQUARE -----------------------------------")
+for i in range(4):
+    if i ==1 or i == 2:
+        print("*"+ " "* 2+ "*")
+    else:
+        print("****")
+    
+
+print("-------------------------------------ROMBUS------------------------------------")
+
+#ROMBUS
+for i in range(1,5):
+    print(" "*(4-i)+i*"*"+(i-1)*"*")
+for i in range(3,0,-1):
+    print(" "*(4-i)+i*"*"+(i-1)*"*")
